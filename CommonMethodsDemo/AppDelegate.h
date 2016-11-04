2144d0fdb5e571ef4dbdafac73af04f8b043f971
阳永辉 <hopeyyh@qq.com>
2016-11-04 20:40:11 +0800

    Initial Commit

---
+++ CommonMethodsDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CommonMethodsDemo
+//
+//  Created by 阳永辉 on 2016/11/4.
+//  Copyright © 2016年 yangyonghui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CommonMethodsDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CommonMethodsDemo
+//
+//  Created by 阳永辉 on 2016/11/4.
+//  Copyright © 2016年 yangyonghui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
